92146e67321bfb3b5000b2d9cdddfe6f6785255c
surya <surya.jain@gmail.com>
2021-10-22 18:22:36 +0530

    inital commit

---
+++ src/test/java/com/surya/firstwebapp/FirstwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surya.firstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
